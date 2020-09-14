df49677a534d98fb1d0d8219709d95d662f64ab7
Thierry DECKER <mail@thierry-decker.com>
2020-09-14 17:03:10 +0200

    Initial commit

---
+++ siarnaq/__init__.py
@@ -0,0 +1,15 @@
+"""Module name.
+
+Copyright (c) 2020 Thierry P.G. DECKER
+All Rights Reserved.
+Released under the MIT license
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
