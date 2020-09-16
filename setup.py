66467124891bdcec9dd98d7650695469bb7fcedc
Thierry DECKER <mail@thierry-decker.com>
2020-09-16 16:58:17 +0200

    Add setup.py for PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="siarnaq",
+        version="0.1.1",
+        author="Thierry P.G. DECKER",
+        author_email="mail@thierry-decker.com",
+        description="A conversion library",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/thierrydecker/siarnaq",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.8',
+)
