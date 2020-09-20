95c546870aa137ec7876f8133afa08bc1398f505
Thierry DECKER <mail@thierry-decker.com>
2020-09-20 16:30:48 +0200

    Add planeangles.py add PlaneAngle class Add conv_de_to_gr static method Add conv_de_to_mi static method Add conv_de_to_ma static method Add conv_de_to_sa static method Add conv_gr_to_de static method Add conv_gr_to_mi static method Add conv_gr_to_ma static method Add conv_gr_to_ra static method Add conv_gr_to_sa static method Add conv_mi_to_de static method Add conv_mi_to_gr static method Add conv_mi_to_ma static method Add conv_mi_to_ra static method Add conv_mi_to_sa static method

---
+++ siarnaq/planeangles.py
@@ -0,0 +1,211 @@
+"""Plane angles conversions.
+
+Copyright (c) 2020 Thierry P.G. DECKER
+All Rights Reserved.
+Released under the MIT license
+
+"""
+import math
+
+
+class PlaneAngle:
+    """Plane angles class.
+
+    """
+    _scales = {
+        'de',  # Degree
+        'gr',  # Gradiant
+        'mi',  # Milliradiant
+        'ma',  # Minute of arc
+        'ra',  # Radian
+        'sa',  # Second of arc
+    }
+
+    @property
+    def scales(self):
+        """Supported plane angles scales.
+
+        Returns:
+            A set of the managed scales.
+        """
+        return self._scales
+
+    @staticmethod
+    def conv_de_to_gr(angle):
+        """Convert Degree value to Gradiant.
+
+        Args:
+            angle: A float containing the Degree value to convert.
+
+        Returns:
+            A float containing the Gradiant value.
+        """
+        return angle * 200 / 180
+
+    @staticmethod
+    def conv_de_to_mi(angle):
+        """Convert Degree value to Milliradian.
+
+        Args:
+            angle: A float containing the Degree value to convert.
+
+        Returns:
+            A float containing the Milliradian value.
+        """
+        return angle * 1000 * math.pi / 180
+
+    @staticmethod
+    def conv_de_to_ma(angle):
+        """Convert Degree value to Minute of arc.
+
+        Args:
+            angle: A float containing the Degree value to convert.
+
+        Returns:
+            A float containing the Minute of arc value.
+        """
+        return angle * 60
+
+    @staticmethod
+    def conv_de_to_ra(angle):
+        """Convert Degree value to Radian.
+
+        Args:
+            angle: A float containing the Degree value to convert.
+
+        Returns:
+            A float containing the Radian value.
+        """
+        return angle * math.pi / 180
+
+    @staticmethod
+    def conv_de_to_sa(angle):
+        """Convert Degree value to Second of arc.
+
+        Args:
+            angle: A float containing the Degree value to convert.
+
+        Returns:
+            A float containing the Second of arc value.
+        """
+        return angle * 3600
+
+    @staticmethod
+    def conv_gr_to_de(angle):
+        """Convert Gradian value to Degree.
+
+        Args:
+            angle: A float containing the Gradian value to convert.
+
+        Returns:
+            A float containing the Degree value.
+        """
+        return angle * 180 / 200
+
+    @staticmethod
+    def conv_gr_to_mi(angle):
+        """Convert Gradian value to Milliradian.
+
+        Args:
+            angle: A float containing the Gradian value to convert.
+
+        Returns:
+            A float containing the Milliradian value.
+        """
+        return angle * 1000 * math.pi / 200
+
+    @staticmethod
+    def conv_gr_to_ma(angle):
+        """Convert Gradian value to Minute of arc.
+
+        Args:
+            angle: A float containing the Gradian value to convert.
+
+        Returns:
+            A float containing the Minute of arc value.
+        """
+        return angle * 54
+
+    @staticmethod
+    def conv_gr_to_ra(angle):
+        """Convert Gradian value to Radian.
+
+        Args:
+            angle: A float containing the Gradian value to convert.
+
+        Returns:
+            A float containing the Radian value.
+        """
+        return angle * math.pi / 200
+
+    @staticmethod
+    def conv_gr_to_sa(angle):
+        """Convert Gradian value to Second of arc.
+
+        Args:
+            angle: A float containing the Gradian value to convert.
+
+        Returns:
+            A float containing the Second of arc value.
+        """
+        return angle * 3240
+
+    @staticmethod
+    def conv_mi_to_de(angle):
+        """Convert Milliradian value to Degree.
+
+        Args:
+            angle: A float containing the Milliradian value to convert.
+
+        Returns:
+            A float containing the Degree value.
+        """
+        return angle * 180 / 1000 / math.pi
+
+    @staticmethod
+    def conv_mi_to_gr(angle):
+        """Convert Milliradian value to Gradian.
+
+        Args:
+            angle: A float containing the Milliradian value to convert.
+
+        Returns:
+            A float containing the Gradian value.
+        """
+        return angle * 200 / 1000 / math.pi
+
+    @staticmethod
+    def conv_mi_to_ma(angle):
+        """Convert Milliradian value to Minute of arc.
+
+        Args:
+            angle: A float containing the Milliradian value to convert.
+
+        Returns:
+            A float containing the Minute of arc value.
+        """
+        return angle * 60 * 180 / 1000 / math.pi
+
+    @staticmethod
+    def conv_mi_to_ra(angle):
+        """Convert Milliradian value to Radian.
+
+        Args:
+            angle: A float containing the Milliradian value to convert.
+
+        Returns:
+            A float containing the Radian value.
+        """
+        return angle / 1000
+
+    @staticmethod
+    def conv_mi_to_sa(angle):
+        """Convert Milliradian value to Second of arc.
+
+        Args:
+            angle: A float containing the Milliradian value to convert.
+
+        Returns:
+            A float containing the Second of arc value.
+        """
+        return angle * (3600 * 180) / (1000 * math.pi)
---
+++ tests/test_planeangles.py
@@ -0,0 +1,73 @@
+"""Plane angles tests module.
+
+Copyright (c) 2020 Thierry P.G. DECKER
+All Rights Reserved.
+Released under the MIT license
+
+"""
+
+from siarnaq.planeangles import PlaneAngle
+
+
+def test_property_getter_scales():
+    assert PlaneAngle().scales == {'de', 'gr', 'mi', 'ma', 'ra', 'sa'}
+
+
+def test_conv_de_to_gr():
+    assert round(PlaneAngle.conv_de_to_gr(1), 2) == 1.11
+
+
+def test_conv_de_to_mi():
+    assert round(PlaneAngle.conv_de_to_mi(1), 2) == 17.45
+
+
+def test_conv_de_to_ma():
+    assert round(PlaneAngle.conv_de_to_ma(1), 2) == 60
+
+
+def test_conv_de_to_ra():
+    assert round(PlaneAngle.conv_de_to_ra(1), 2) == 0.02
+
+
+def test_conv_de_to_sa():
+    assert round(PlaneAngle.conv_de_to_sa(1), 2) == 3600
+
+
+def test_conv_gr_to_de():
+    assert round(PlaneAngle.conv_gr_to_de(1), 2) == 0.9
+
+
+def test_conv_gr_to_mi():
+    assert round(PlaneAngle.conv_gr_to_mi(1), 2) == 15.71
+
+
+def test_conv_gr_to_ma():
+    assert round(PlaneAngle.conv_gr_to_ma(1), 2) == 54
+
+
+def test_conv_gr_to_ra():
+    assert round(PlaneAngle.conv_gr_to_ra(1), 2) == 0.02
+
+
+def test_conv_gr_to_sa():
+    assert round(PlaneAngle.conv_gr_to_sa(1), 2) == 3240
+
+
+def test_conv_mi_to_de():
+    assert round(PlaneAngle.conv_mi_to_de(1), 2) == 0.06
+
+
+def test_conv_mi_to_gr():
+    assert round(PlaneAngle.conv_mi_to_gr(1), 2) == 0.06
+
+
+def test_conv_mi_to_ma():
+    assert round(PlaneAngle.conv_mi_to_ma(1), 2) == 3.44
+
+
+def test_conv_mi_to_ra():
+    assert round(PlaneAngle.conv_mi_to_ra(1000), 2) == 1.00
+
+
+def test_conv_mi_to_sa():
+    assert round(PlaneAngle.conv_mi_to_sa(1), 2) == 206.26
